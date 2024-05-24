a5f0c494387e239138e63e94a20e6b8d67bd76cf
Tom Smith <tomdsmith1@gmail.com>
2024-05-24 10:25:13 +0100

    Update api endpoint

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,2 @@
 export const CONF_FILE_NAME = ".nodana.conf";
-export const API_BASE_URL = "http://localhost:3004/v1"; // 'https://api.nodana.io/v1'
+export const API_BASE_URL = "https://api.nodana.io/v1";
